19eb693b7ac30d3809731a3059bb3a6b81758d43
aswinsekar <aswinsekar20@gmail.com>
2020-05-11 23:10:03 +0530

    Problem 5 Solution

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -2,6 +2,7 @@ mod prob1;
 mod prob2;
 mod prob3;
 mod prob4;
+mod prob5;
 
 pub fn solution(prob_number: u16) {
     match prob_number {
@@ -9,6 +10,7 @@ pub fn solution(prob_number: u16) {
         2 => prob2::solution(),
         3 => prob3::solution(),
         4 => prob4::solution(),
+        5 => prob5::solution(),
         _ => println!("Solution doesn't exist or problem number is out of range"),
     }
 }
